646982c8ad55e84275d86ff1ea760ce653d74c2a
Will Conklin <will.conklin@gmail.com>
2021-10-13 21:11:14 -0400

    Start terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.62.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_kms_key" "will-dev-s3-kms" {
+    description = "will-dev-s3-kms-key"
+}
+
+resource "aws_s3_bucket" "will-dev" {
+    bucket = "s3-will-dev"
+
+    server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.will-dev-s3-kms.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  
+}
