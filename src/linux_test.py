19453273ec76690a31f718c780a9de5db86db056
Will Conklin <will.conklin@gmail.com>
2021-10-17 22:41:44 -0400

    Quick AWS test

--- src/linux_test.py
+++ src/linux_test.py
@@ -1,1 +1,7 @@
-print("Hello")
+import boto3
+
+session = boto3.Session(region_name='us-east-1')
+
+client = session.client('s3')
+
+client.put_object(Body=b'testing',Bucket='s3-will-dev',Key='test.txt')
