37ceb0ef7ebf67403360356d063a5b0cfd47bbc1
Will Conklin <will.conklin@gmail.com>
2021-10-18 22:33:43 -0400

    Move to modules

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,30 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.62.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-resource "aws_kms_key" "will-dev-s3-kms" {
-    description = "will-dev-s3-kms-key"
-}
-
-resource "aws_s3_bucket" "will-dev" {
-    bucket = "s3-will-dev"
-
-    server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.will-dev-s3-kms.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-  
+module "s3" {
+  source = "./modules/s3"
 }
